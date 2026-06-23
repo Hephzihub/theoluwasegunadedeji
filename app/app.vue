bb61f7154090579b80ea23aff50aa9ca5dbe90e3
Oluwasegun Adedeji <oluwasheges@gmail.com>
2026-06-23 13:09:11 +0100

    Update app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen">
     <SiteHeader />
-    <main>
+    <main class="bg-[radial-gradient(circle,#ebebeb_1px,transparent_1px)]" style="background-size: 28px 28px;">
       <HeroSection />
       <SectionDivider />
       <AboutSection />
